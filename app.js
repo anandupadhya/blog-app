c45761ddbd38591e31c9a4c48651778e611ebd1c
Anand Upadhya <anand.upadhya@yahoo.com>
2025-04-11 10:32:57 +0000

    create profile routes and page

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const commentRoutes = require('./routes/commentRoutes')
 const userRoutes = require('./routes/userRoutes')
 const authRoutes = require('./routes/authRoutes')
 
+const User = require("./models/User")
+
 const session = require('express-session');
 const passport = require('passport');
 require('./config/passport'); // make sure to import your passport config
@@ -43,6 +45,35 @@ app.use("/posts", postRoutes)
 app.use("/comments", commentRoutes)
 app.use("/users", userRoutes)
 app.use("/auth", authRoutes)
+
+app.get("/profile", (req, res) => {
+	if (!req.user) {
+		res.send("You are not logged in!")
+	}
+	
+	res.render("profile/profile")
+})
+
+app.patch("/profile", async (req, res) => {
+	if (!req.user) {
+		res.send("You are not logged in!")
+	}
+	// console.log("Trying to update profile")
+	// console.log(req.body)
+	// console.log(req.user.id)
+	
+	const newUsername = req.body.username
+	const newFirstName = req.body.firstName
+	const newLastName = req.body.lastName
+	const newEmail = req.body.email
+	
+	await User.findByIdAndUpdate(
+		req.user.id,
+		{ $set: { username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail } }
+	)
+	
+	res.redirect("/profile")
+})
 
 app.listen(3000, () => {
 	console.log("SERVER IS RUNNING")
