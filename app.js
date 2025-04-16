1a488ed1aa7f8832fd269a2a388ea9e158ff34d6
Anand Upadhya <anand.upadhya@yahoo.com>
2025-04-16 03:02:21 +0000

    move profile routes code to userRoutes.js and fix links inpost index page and user profile page

--- app.js
+++ app.js
@@ -9,12 +9,9 @@ const commentRoutes = require('./routes/commentRoutes')
 const userRoutes = require('./routes/userRoutes')
 const authRoutes = require('./routes/authRoutes')
 
-const User = require("./models/User")
-const Post = require("./models/Post")
-
-const session = require('express-session');
-const passport = require('passport');
-require('./config/passport'); // make sure to import your passport config
+const session = require('express-session')
+const passport = require('passport')
+require('./config/passport')
 
 const app = express()
 connectDB()
@@ -30,13 +27,13 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
-});
+})
 
 app.get("/", (req, res) => {
 	res.redirect("/posts")
@@ -46,37 +43,6 @@ app.use("/posts", postRoutes)
 app.use("/comments", commentRoutes)
 app.use("/users", userRoutes)
 app.use("/auth", authRoutes)
-
-app.get("/profile", async (req, res) => {
-	if (!req.user) {
-		res.send("You are not logged in!")
-	}
-	
-	const userPosts = await Post.find({ author: req.user })
-	console.log(userPosts)
-	res.render("profile/profile", { userPosts })
-})
-
-app.patch("/profile", async (req, res) => {
-	if (!req.user) {
-		res.send("You are not logged in!")
-	}
-	// console.log("Trying to update profile")
-	// console.log(req.body)
-	// console.log(req.user.id)
-	
-	const newUsername = req.body.username
-	const newFirstName = req.body.firstName
-	const newLastName = req.body.lastName
-	const newEmail = req.body.email
-	
-	await User.findByIdAndUpdate(
-		req.user.id,
-		{ $set: { username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail } }
-	)
-	
-	res.redirect("/profile")
-})
 
 app.listen(3000, () => {
 	console.log("SERVER IS RUNNING")
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,37 @@ const express = require("express")
 const router = express.Router()
 
 const User = require('../models/User')
+const Post = require('../models/Post')
 
-// REGISTER
+router.get("/profile", async (req, res) => {
+	if (!req.user) {
+		res.send("You are not logged in!")
+	}
+	
+	const userPosts = await Post.find({ author: req.user })
+	console.log(userPosts)
+	res.render("profile/profile", { userPosts })
+})
+
+router.patch("/profile", async (req, res) => {
+	if (!req.user) {
+		res.send("You are not logged in!")
+	}
+	// console.log("Trying to update profile")
+	// console.log(req.body)
+	// console.log(req.user.id)
+	
+	const newUsername = req.body.username
+	const newFirstName = req.body.firstName
+	const newLastName = req.body.lastName
+	const newEmail = req.body.email
+	
+	await User.findByIdAndUpdate(
+		req.user.id,
+		{ $set: { username: newUsername, firstName: newFirstName, lastName: newLastName, email: newEmail } }
+	)
+	
+	res.redirect("/users/profile")
+})
 
 module.exports = router
